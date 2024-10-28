17b8ffdca26383a69b56caea3e8280c408c81904
Marius Calin <marius.calin0699@gmail.com>
2024-10-28 21:00:21 +0200

    Complete Tic-Tac-Toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,6 @@
-
 board = ["-", "-", "-",
          "-","-","-",
          "-","-","-"]
-
 gameIsOn = True
 current_player = ""
 
@@ -13,7 +11,7 @@ def display_board():
 
 display_board()
 
-def select_sign():
+def start_game():
     global current_player
     choice = input("Select Player - X or O:\n")
     if choice == "X" or choice == "x":
@@ -23,7 +21,7 @@ def select_sign():
         p1 = "O"
         play_game(p1)
     else:
-        return select_sign()
+        return start_game()
 
 def switch_player():
     global current_player
@@ -36,50 +34,59 @@ def switch_player():
 def play_game(p1):
     global gameIsOn
     global current_player
-    player1 =p1
-
-
-    current_player = player1
-    print("Current player " +current_player)
-    position = input("Choose position from 1 - 9: ")
+    global board
+    current_player = p1
 
     while gameIsOn:
         print("Current player " +current_player)
+        position = input("Choose position from 1 - 9: ")
         while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             position = input("Choose position from 1 - 9: ")
         position = int(position) - 1
         if board[position] == "-":
             board[position] = current_player
+            display_board()
+            if board[0] == board[1] == board[2] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[0] + " you WON!")
+            elif board[3] == board[4] == board[5] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[3] + " you WON!")
+            elif board[6] == board[7] == board[8] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[6] + " you WON!")
+            elif board[0] == board[3] == board[6] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[0] + " you WON!")
+            elif board[1] == board[4] == board[7] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[1] + " you WON!")
+            elif board[2] == board[5] == board[8] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[2] + " you WON!")
+            elif board[0] == board[4] == board[8] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[0] + " you WON!")
+            elif board[2] == board[4] == board[6] != "-":
+                gameIsOn = False
+                print("Congratulations " + board[6] + " you WON!")
+            elif "-" not in board:
+                gameIsOn = False
+                print("It's a Tie")
+            current_player = switch_player()
         else:
             print("Position already selected, choose another position!")
-        display_board()
-        if board[0] == board[1] == board[2] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[0] + " you WON!")
-        elif board[3] == board[4] == board[5] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[3] + " you WON!")
-        elif board[6] == board[7] == board[8] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[6] + " you WON!")
-        elif board[0] == board[3] == board[6] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[0] + " you WON!")
-        elif board[1] == board[4] == board[7] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[1] + " you WON!")
-        elif board[2] == board[5] == board[8] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[2] + " you WON!")
-        elif board[0] == board[4] == board[8] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[0] + " you WON!")
-        elif board[2] == board[4] == board[6] != "-":
-            gameIsOn = False
-            print("Congratulations " + board[6] + " you WON!")
-        elif "-" not in board:
-            gameIsOn = False
-            print("It's a Tie")
-        current_player=switch_player()
+    if not gameIsOn:
+        restart = input("Want to go again? Press Y or N\n")
+        while restart not in ["y","Y","N","n"]:
+            restart = input("Want to go again? Press Y or N\n")
+        if restart == "Y" or restart == "y":
+            gameIsOn = True
+            board = ["-", "-", "-",
+                     "-", "-", "-",
+                     "-", "-", "-"]
+            start_game()
+        elif restart == "N" or restart == "n":
+            print("See you next time!")
 
-select_sign()
+start_game()
